1e654a9d07d5cee7ef3f88bec3f45fc38b88e338
Yen-hua Chuang <chuangyen@old-bailey.ecs.vuw.ac.nz>
2014-07-31 16:27:46 +1200

    some stupid commit

--- elearningserver.php
+++ elearningserver.php
@@ -12,7 +12,8 @@ function checkPassword($username,$password){
 	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
 	$dbconnection = pg_connect($connectionString);
 	$result = pg_query($dbconnection,"SELECT password FROM users WHERE username='eman123'");
-	return $result;
+	$row = pg_fetch_row($result);
+	return $result[0];
 	
 
 }
