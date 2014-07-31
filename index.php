0864d43b91bae3d63f69a6ef7b6c2df5c5e60f12
Yen-hua Chuang <chuangyen@old-bailey.ecs.vuw.ac.nz>
2014-07-31 15:23:21 +1200

    adding stolen codde from web

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <head><title>I am afish</title></head>
 <body>
-<?php header('Access-Control-Allow-Origin: *');
+<?php 
 echo "hello world";
 
 function func1(){
@@ -13,11 +13,19 @@ function func2(){
 }
 
 function checkPassWord($username,$password){
-	return $username==$password;
+	return $username==$password;.
 	return "chekcLogin";
 }
 	
 if(isset($_POST['funcName'])){
+	switch($_POST['funcName']){
+		case 'func1':
+			echo(func1());
+			break;	
+		case 'func2':
+			echo(func2());
+			break;
+	}
   echo $_POST['funcName'];
 }
 ?>
