b266c81eccdb0adf1ce3b9b6d35b0cabf33d9494
Yen-hua Chuang <chuangyen@old-bailey.ecs.vuw.ac.nz>
2014-07-31 14:33:13 +1200

    adding stolen codde from web

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
 <?php header('Access-Control-Allow-Origin: *'); 
-echo "hello world";
+//echo "hello world";
+
+function func1(){
+	return "this is function1";
+}
+
+function func2(){
+	return "this is function2";
+}
+
+//function checkPassWord($username,$password){
+	//return $username==$password;
+	//return "chekcLogin";
+//}
+	
+	if(isset($_POST['funcName'])){
+	$fn = $_POST['funcName'];
+	switch($fn){
+		case 'func1':
+			echo func1();
+			break;
+			
+		case 'func2':
+			echo func2();
+			break;
+	}	
+	}
+		//case 'tryLogin':
+		//	echo "checkPassword";			
+			//$username = $_POST['username'];
+			//$password = $_POST['password'];
+			//echo checkPassWord($username,$password);
+			//break;
+			
+}
+}
 ?>
