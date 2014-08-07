d1175c9c118ca627cf9a6d0e847ba85650eb9544
Chris Hawkins <chrishawkinsnz@gmail.com>
2014-08-07 12:51:50 +1200

    here is a commit

--- loginAdmin.php
+++ loginAdmin.php
@@ -4,7 +4,7 @@
 function checkPasswordAdmin($username,$password){
 	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
 	$dbconnection = pg_connect($connectionString);
-	$result = pg_query($dbconnection,"SELECT password FROM users2 WHERE username='".$username."' AND accessLevel='admin'");
+	$result = pg_query($dbconnection,"SELECT password FROM useraccounts WHERE username='".$username."' AND isadmin='t'");
 	$row = pg_fetch_row($result);
 	$realPassword = $row[0];
 	return ($realPassword==$password) ? 'true' : 'false';
