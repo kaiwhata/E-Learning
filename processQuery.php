e4e724255af96e93fa9b8a2ed8cf95f9eb9e6f72
Chris Hawkins <chrishawkinsnz@gmail.com>
2014-08-07 13:43:39 +1200

    here is a commit

--- processQuery.php
+++ processQuery.php
@@ -1,17 +1,10 @@
 <?php header('Access-Control-Allow-Origin: *');
 
-function func1(){
-	return "this is function1";
-}
 
-function func2(){
-	return "this is function2";
-}
-
-function checkPassword($username,$password){
+function checkPasswordAdmin($username,$password){
 	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
 	$dbconnection = pg_connect($connectionString);
-	$result = pg_query($dbconnection,"SELECT password FROM useraccounts WHERE username='".$username."'");
+	$result = pg_query($dbconnection,"SELECT password FROM useraccounts WHERE username='".$username."' AND isadmin='t'");
 	$row = pg_fetch_row($result);
 	$realPassword = $row[0];
 	return ($realPassword==$password) ? 'true' : 'false';
@@ -23,14 +16,8 @@ function checkPassword($username,$password){
 	
 if(isset($_POST['funcName'])){
 	switch($_POST['funcName']){
-		case 'func1':
-			echo(func1());
-			break;	
-		case 'func2':
-			echo(func2());
-			break;
-		case 'checkPassword':
-			echo(checkPassword($_POST['username'],$_POST['password']));
+		case 'checkPasswordAdmin':
+			echo(checkPasswordAdmin($_POST['username'],$_POST['password']));
 			break;
 	}
 }
