b5f7de994cccde48281468cd7650fc37e9a7c2a5
Chris Hawkins <chrishawkinsnz@gmail.com>
2014-08-07 14:07:01 +1200

    here is a commit

--- processQuery.php
+++ processQuery.php
@@ -14,7 +14,7 @@ function checkPasswordAdmin($username,$password){
 
 }
 
-function getAllQuestions($username,$password){
+function getAllQuestions(){
 	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v 			user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
 	$dbconnection = pg_connect($connectionString);
 	$result = pg_query($dbconnection,"SELECT * FROM questions");
