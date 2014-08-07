cfa7df87f7cb9e1818d106e24fbc0bdc5281eab3
Chris Hawkins <chrishawkinsnz@gmail.com>
2014-08-07 14:39:13 +1200

    here is a commit

--- processQuery.php
+++ processQuery.php
@@ -17,7 +17,7 @@ function checkPasswordAdmin($username,$password){
 function getAllQuestions(){
 	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v 			user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
 	$dbconnection = pg_connect($connectionString);
-	$result = pg_query($dbconnection,"SELECT * FROM question INNER JOIN possibleanswers ON (question.panswerid = possibleanswers.id");
+	$result = pg_query($dbconnection,"SELECT * FROM question INNER JOIN possibleanswers ON (question.panswerid = possibleanswers.id)");
 
 	$row = pg_fetch_row($result);
 	return json_encode(array_values($row));
