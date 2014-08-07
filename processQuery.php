b4a9a706c08c192f5304b4fd36422481e8482c59
Chris Hawkins <chrishawkinsnz@gmail.com>
2014-08-07 14:04:36 +1200

    here is a commit

--- processQuery.php
+++ processQuery.php
@@ -18,6 +18,8 @@ function getAllQuestions($username,$password){
 	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v 			user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
 	$dbconnection = pg_connect($connectionString);
 	$result = pg_query($dbconnection,"SELECT * FROM questions");
+	$row = pg_fetch_row($result);
+	return $row;
 	return json_encode(array_values(pg_fetch_row($result)));
 }
 	
