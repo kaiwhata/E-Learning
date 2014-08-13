3830622d7e69397588e83ad1a6dd39e3eaf18074
gekenel <alexc1@windowslive.com>
2014-08-14 10:18:43 +1200

    here is a commit

--- js/controllers.js
+++ js/controllers.js
@@ -5,43 +5,44 @@ questionList
 				'questionCtrl',
 				function questionCtrl($scope) {
 					$scope.questions = [];
-					$scope.answer = false;
-					$scope.answer1;
-					$scope.answer2;
-					$scope.answer3;
+//					$scope.answer = false;
+//					$scope.answer1;
+//					$scope.answer2;
+//					$scope.answer3;
 
-					// Make dummy multiChoiceQuestions
-					//
-					var mcq = new MultiChoiceQuestion("1", "what is apples?",
-							1, [ "banana", "multiple apple", "pear" ]);
-					var mcq1 = new MultiChoiceQuestion("2", "what is banana?",
-							0, [ "banana", "multiple apples", "pear" ]);
-					var mcq2 = new MultiChoiceQuestion("3", "what is pear?", 2,
-							[ "banana", "multiple apple", "pear" ]);
+//					// Make dummy multiChoiceQuestions
+//					//
+//					var mcq = new MultiChoiceQuestion("1", "what is apples?",
+//							1, [ "banana", "multiple apple", "pear" ]);
+//					var mcq1 = new MultiChoiceQuestion("2", "what is banana?",
+//							0, [ "banana", "multiple apples", "pear" ]);
+//					var mcq2 = new MultiChoiceQuestion("3", "what is pear?", 2,
+//							[ "banana", "multiple apple", "pear" ]);
+//
+//					$scope.questions.push(mcq);
+//					$scope.questions.push(mcq1);
+//					$scope.questions.push(mcq2);
+//
+//					// Make Dummy Text Questions
+//					var teq1 = new TextEntryQuestion("4",
+//							"How do you greet people in French?", [ "salut",
+//									"bonjour" ])
+//					var teq2 = new TextEntryQuestion(
+//							"5",
+//							"Complete the sentnce: We have lots of _____ at the arcade",
+//							[ "games" ])
+//					//
+//					$scope.questions.push(teq1);
+//					$scope.questions.push(teq2);
+//
+//					// Make Dummy Number Question
+//					var neq1 = new NumberEntryQuestion("6", "What is 10+10?",
+//							20);
+//					var neq2 = new NumberEntryToleranceQuestion("7",
+//							"What is 1/3?", 0.333, 0.0015);
+//					$scope.questions.push(neq1);
+//					$scope.questions.push(neq2);
 
-					$scope.questions.push(mcq);
-					$scope.questions.push(mcq1);
-					$scope.questions.push(mcq2);
-
-					// Make Dummy Text Questions
-					var teq1 = new TextEntryQuestion("4",
-							"How do you greet people in French?", [ "salut",
-									"bonjour" ])
-					var teq2 = new TextEntryQuestion(
-							"5",
-							"Complete the sentnce: We have lots of _____ at the arcade",
-							[ "games" ])
-					//
-					$scope.questions.push(teq1);
-					$scope.questions.push(teq2);
-
-					// Make Dummy Number Question
-					var neq1 = new NumberEntryQuestion("6", "What is 10+10?",
-							20);
-					var neq2 = new NumberEntryToleranceQuestion("7",
-							"What is 1/3?", 0.333, 0.0015);
-					$scope.questions.push(neq1);
-					$scope.questions.push(neq2);
 
 					/**
 					 * Check all the answers of questions and alert the user if
