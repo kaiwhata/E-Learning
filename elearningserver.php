dfc7b8556c2951e38dd40be68143493e9016f5ca
Chris Hawkins <chrishawkinsnz@gmail.com>
2014-08-14 09:54:15 +1200

    new quiz php function

--- elearningserver.php
+++ elearningserver.php
@@ -21,6 +21,15 @@ function checkPassword($username,$password){
 
 }
 
+function getQuizes(){
+	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
+	$dbconnection = pg_connect($connectionString);
+	$result = pg_query($dbconnection,"SELECT * FROM quiz;");
+	$row = pg_fetch_row($result);
+
+	return $row;
+}
+
 function sendResult() {
 	
 }
@@ -37,6 +46,9 @@ if(isset($_POST['funcName'])){
 			echo(checkPassword($_POST['username'],$_POST['password']));
 			break;
 		case 'sendResult':
+			break;
+		case 'getQuizes':
+			echo(getQuizes());		
 			break;
 	}
 }
