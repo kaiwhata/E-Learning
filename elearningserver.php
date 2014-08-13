b666dd2915595d7a3396aa55c6aa02133bbb5869
Chris Hawkins <chrishawkinsnz@gmail.com>
2014-08-14 10:40:41 +1200

    php update

--- elearningserver.php
+++ elearningserver.php
@@ -25,11 +25,12 @@ function getQuizes(){
 	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
 	$dbconnection = pg_connect($connectionString);
 	$result = pg_query($dbconnection,"SELECT * FROM quiz;");
-	$row = pg_fetch_row($result);
+	$row = pg_fetch_all($result);
 	$bigline = "";
 	foreach($row as $line){
 	$bigline .= "*";
 	$bigline .= $line;
+	
 
 	}
 
