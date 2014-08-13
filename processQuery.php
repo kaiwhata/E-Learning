0de7a7f95d7b38f0fe1135b3d922437edea69480
gekenel <alexc1@windowslive.com>
2014-08-14 11:28:18 +1200

    here is a commit

--- processQuery.php
+++ processQuery.php
@@ -42,7 +42,7 @@ function getAllQuestionsFromQuiz($quizname){
 	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v 			user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
 	$dbconnection = pg_connect ( $connectionString );
 	$result = pg_query ( $dbconnection, "select row_to_json(row)
-from(SELECT * FROM quiz INNER JOIN question ON  (quiz.name=question.quizname) LEFT OUTER JOIN possibleanswers ON (question.panswerid = possibleanswers.id) WHERE(quiz.name='Dummy Test') row;" );
+from((SELECT * FROM quiz INNER JOIN question ON  (quiz.name=question.quizname) LEFT OUTER JOIN possibleanswers ON (question.panswerid = possibleanswers.id) WHERE(quiz.name='Dummy Test')) row;" );
 	return json_encode ( pg_fetch_all ( $result ) );
 }
 
