256613b1d1ac9c28a649bfe4698c9cf758e333a6
Sohaila Sahib <sahibsoha@old-bailey.ecs.vuw.ac.nz>
2014-08-14 09:58:50 +1200

    message

--- processQuery2.php
+++ processQuery2.php
@@ -4,7 +4,7 @@
 function checkPasswordAdmin($username,$password){
 	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
 	$dbconnection = pg_connect($connectionString);
-	$result = pg_query($dbconnection,"SELECT password FROM useraccounts WHERE username='".$username."' AND isadmin='t'");
+	$result = pg_query($dbconnection,"SELECT password FROM useraccount WHERE username='".$username."' AND isadmin='t'");
 	$row = pg_fetch_row($result);
 	$realPassword = $row[0];
 	return ($realPassword==$password) ? 'true' : 'false';
@@ -19,11 +19,22 @@ function getAllQuestions(){
 	$dbconnection = pg_connect($connectionString);
 
 	$result = pg_query($dbconnection,"select row_to_json(row)
-from(select * from question INNER JOIN possibleanswers ON (question.panswerid = possibleanswers.id))row;");
+from(select * from question LEFT OUTER JOIN possibleanswers ON (question.panswerid = possibleanswers.id))row;");
 
+//	$str = "{";
+//	for($i = 0;$i<2;$i++){
+//		$str.=json_encode(pg_fetch_row($result));
+//		if($i<1){
+//			$str.=",";
+//		}
+//	}
 
-	$row = pg_fetch_row($result);
-	return json_encode(array_values($row));
+//	$str.="}";
+
+//	return $str;
+	//$row = pg_fetch_row($result);
+//	return (pg_fetch_all($result));
+	return json_encode(pg_fetch_all($result));
 }
 
 function getAllQuestionsWithoutOptions(){
