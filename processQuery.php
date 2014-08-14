86a5c24b0f55cf902a438ee520bd61b2ef66dee5
gekenel <alexc1@windowslive.com>
2014-08-14 13:55:59 +1200

    here is a commit

--- processQuery.php
+++ processQuery.php
@@ -18,7 +18,11 @@ function getAllQuestions(){
 	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v 			user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
 	$dbconnection = pg_connect($connectionString);
 
-	$result = pg_query($dbconnection," select row_to_json(row) from (SELECT * FROM result WHERE userid=1) row")
+
+	$id = pg_query ( $dbconnection,"SELECT id FROM useraccount WHERE username='$username'");
+	$userid = pg_fetch_row($id)[0];
+
+	$result = pg_query($dbconnection," select row_to_json(row) from (SELECT * FROM result WHERE userid=$userid) row");
 
 //	$str = "{";
 //	for($i = 0;$i<2;$i++){
