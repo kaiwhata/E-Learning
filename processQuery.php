9a7c947383c270d4f922c25743e7e4a3530b4cdd
gekenel <alexc1@windowslive.com>
2014-08-14 13:13:25 +1200

    here is a commit

--- processQuery.php
+++ processQuery.php
@@ -56,7 +56,7 @@ function sendResults($username,$password,$quizname,$score){
 	//$id = pg_query ( $dbconnection,"SELECT id FROM useraccount WHERE username=".$username.";");
 	pg_query ( $dbconnection,"INSERT INTO result (userid,quizname, score) VALUES (1,'".$quizname."',1)");
 
-	return "hi";
+	return $score;
 
 }
 
