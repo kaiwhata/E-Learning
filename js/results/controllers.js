29b1f867a4f4042bfcde34287792c1bc045605b2
gekenel <alexc1@windowslive.com>
2014-08-14 13:46:17 +1200

    here is a commit

--- js/results/controllers.js
+++ js/results/controllers.js
@@ -14,69 +14,7 @@ results.controller('resultsCtrl', function questionCtrl($scope) {
 						"password" : sessionStorage.getItem('password')
 					},
 					success : function(response) {
-						console.log(response)
-
-						var array = response;
-
-						var questionArray = [];
-						for (var i = 0; i < JSON.parse(array).length; i++) {
-							var questionJSON = JSON
-									.parse(JSON.parse(array)[i]["row_to_json"]);
-							console.log(questionJSON);
-							questionArray.push(questionJSON);
-
-							// MULTI TYPE
-							if (questionJSON.type == 0) {
-								var answer = questionJSON["canswer"];
-								var body = questionJSON["body"];
-								var id = questionJSON["id"];
-
-								var answerIdx = 0;
-
-								var p1 = questionJSON["p1"];
-								if (p1 == answer) {
-									answerIdx = 0;
-								}
-								var p2 = questionJSON["p2"];
-								if (p2 == answer) {
-									answerIdx = 1;
-								}
-								var p3 = questionJSON["p3"];
-								if (p3 == answer) {
-									answerIdx = 2;
-								}
-								var p4 = questionJSON["p4"];
-								if (p4 == answer) {
-									answerIdx = 3;
-								}
-
-								var options = [ p1, p2, p3, p4 ];
-								var question = new MultiChoiceQuestion(
-										id, body, answerIdx,
-										options)
-								$scope.questions.push(question);
-							}
-							// NUMBER TYPE
-							else if (questionJSON.type == 1) {
-								var answer = questionJSON["canswer"];
-								var body = questionJSON["body"];
-								var id = questionJSON["id"];
-								var tol = questionJSON["tolerance"];
-								var question = new NumberEntryToleranceQuestion(
-										id, body, answer, tol);
-								$scope.questions.push(question);
-							}
-							// TEXT TYPE
-							else if (questionJSON.type == 2) {
-								var answer = questionJSON["canswer"];
-								var body = questionJSON["body"];
-								var id = questionJSON["id"];
-								var answerArray = [ answer ];
-								var question = new TextEntryQuestion(
-										id, body, answerArray);
-								$scope.questions.push(question);
-							}
-						}
+						console.log(response);
 						 $scope.$apply();
 					}
 
