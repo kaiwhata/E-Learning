11b8787d622da3a5d78ef3bd78b7e13a4908a4c3
Chris Hawkins <chrishawkinsnz@gmail.com>
2014-08-14 14:11:26 +1200

    finished quiz selection page

--- js/quiz_lib/controllers.js
+++ js/quiz_lib/controllers.js
@@ -18,7 +18,7 @@ quizList.controller('quizCtrl',function quizCtrl($scope) {
 				for(var i=1;i<instanceArray.length;i++){
 					var instanceDeets = instanceArray[i].split(":");
 //					console.log("thing: "+instanceDeets[i]+" "+instanceDeets[i+1]);
-					$scope.quizes.push(instanceDeets[0]+" "+instanceDeets[1]);
+					$scope.quizes.push(instanceDeets[0]+":"+instanceDeets[1]);
 					// create button
 					$scope.loaded=true;
 					$scope.$apply();
@@ -28,5 +28,16 @@ quizList.controller('quizCtrl',function quizCtrl($scope) {
 			}//end success
 		});//end ajax
 	}//end getQuizesFunction
+
+
+	$scope.selectQuiz = function(quizString) {
+		console.log("quizString is "+quizString);
+		var sp = quizString.split(":");
+		var code = sp[0];
+		var quizname = sp[1];
+		sessionStorage.setItem("quizname",quizname);
+		window.location = "http://shrouded-earth-7234.herokuapp.com/indexAng.html"; 
+	}//end func
+
 
 });//end controller
