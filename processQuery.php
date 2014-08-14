44d522942ff0e7598bcf8caad3259c7688b97fad
gekenel <alexc1@windowslive.com>
2014-08-14 14:48:35 +1200

    here is a commit

--- processQuery.php
+++ processQuery.php
@@ -62,8 +62,8 @@ function getResults($username, $password) {
 
 	$result = pg_query ( $dbconnection, " select row_to_json(row) from (SELECT * FROM result WHERE userid=$userid) row" );
 
-	$result = pg_query ( $dbconnection, "select row_to_json(row)
-FROM (SELECT * FROM result WHERE userid=1) row;" );
+// 	$result = pg_query ( $dbconnection, "select row_to_json(row)
+// FROM (SELECT * FROM result WHERE userid=1) row;" );
 
 	return json_encode ( pg_fetch_all ( $result ) );
 }
