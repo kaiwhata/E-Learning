7296748fa93edd2cf969df53e7c7e3befb188d73
gekenel <alexc1@windowslive.com>
2014-08-14 12:34:17 +1200

    here is a commit

--- js/results/controllers.js
+++ js/results/controllers.js
@@ -2,4 +2,23 @@
 
 results.controller('resultsCtrl', function questionCtrl($scope) {
 	$scope.results = ["very good","not very good"];
+
+	$scope.sendResult = = function() {
+		.ajax({
+			url : 'http://shrouded-earth-7234.herokuapp.com/processQuery.php',
+			type : 'post',
+			data : {
+				"funcName" : "sendResults",
+				"username" : "hawkinchri",
+				"password" : "dogs",
+				"quizname" : sessionStorage.getItem('quizname')
+				"score " : 0.75
+			},
+			success : function(response) {
+				console.log(response)
+				 $scope.$apply();
+			}
+
+		});
+	}
 });
