f00a1b65ff418f943a77762694382dd348cbf615
Emmanuel Godinez <godineemma@pulcinella.ecs.vuw.ac.nz>
2014-08-21 10:56:32 +1200

    new function

--- processQuery.php
+++ processQuery.php
@@ -73,6 +73,13 @@ function checkPassword($username,$password){
 	return ($realPassword==$password) ? 'true' : 'false';
 }
 
+function getAllResults(){
+	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
+	$dbconnection = pg_connect($connectionString);
+	$result = pg_query($dbconnection,"SELECT * FROM result");
+	return json_encode(pg_fetch_all($result));
+}
+
 
 if (isset ( $_POST ['funcName'] )) {
 	switch ($_POST ['funcName']) {
@@ -91,6 +98,8 @@ if (isset ( $_POST ['funcName'] )) {
 		case 'checkPassword':
 			echo(checkPassword($_POST['username'],$_POST['password']));
 			break;
+		case 'getAllResults':
+			echo(getAllResults());
 		case 'getQuizzes':
 			echo(checkPassword($_POST['username'],$_POST['password']));
 			break;
