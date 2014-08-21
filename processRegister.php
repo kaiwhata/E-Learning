bfff4caeba5e251031d6acdc43e65ca26e86b2a5
Emmanuel Godinez <godineemma@pulcinella.ecs.vuw.ac.nz>
2014-08-21 13:20:16 +1200

    ladhflaije

--- processRegister.php
+++ processRegister.php
@@ -2,11 +2,12 @@
 
 function Register($username, $email, $password, $fname, $lname) {
 
-	return 'test';
+
 	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
 	$dbconnection = pg_connect($connectionString);
 	//Access table from SQL to insert
 	$result = pg_query($dbconnection,"INSERT INTO useraccounts VALUES ('$fname', '$lname', '$email', '$username', '$password', false);");
+	return 'got to end of php function';
 }
 
 if (isset ( $_POST ['funcName'] )) {
