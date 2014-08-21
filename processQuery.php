a89688489629502d2d1628aa5dae96c7b8327902
gekenel <alexc1@windowslive.com>
2014-08-21 14:29:07 +1200

    change to sql

--- processQuery.php
+++ processQuery.php
@@ -76,7 +76,7 @@ function checkPassword($username,$password){
 function getAllResults(){
 	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
 	$dbconnection = pg_connect($connectionString);
-	$result = pg_query($dbconnection,"SELECT u.fname, u.lname, u.id, r.quizname, r.score, r.timetaken, r.date FROM result r inner join useraccount u on r.userid=u.id");
+	$result = pg_query($dbconnection,"SELECT u.fname, u.lname, u.id, r.quizname, r.score, r.timetaken, r.date FROM result r inner join useraccount u on r.userid=u.id order by score desc");
 	return json_encode(pg_fetch_all($result));
 }
 
