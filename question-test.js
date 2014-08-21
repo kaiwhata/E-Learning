61ec7a0272b6840d08c9e8b38cb069e5e51e8625
gekenel <alexc1@windowslive.com>
2014-08-21 16:59:41 +1200

    sql modified

--- question-test.js
+++ question-test.js
@@ -1,248 +1,137 @@
+<?php
+header ( 'Access-Control-Allow-Origin: *' );
+function checkPasswordAdmin($username, $password) {
+	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
+	$dbconnection = pg_connect ( $connectionString );
+	$result = pg_query ( $dbconnection, "SELECT password FROM useraccount WHERE username='" . $username . "' AND isadmin='t'" );
+	$row = pg_fetch_row ( $result );
+	$realPassword = $row [0];
+	return ($realPassword == $password) ? 'true' : 'false';
+
+}
+
+function checkPassword($username,$password){
+	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
+	$dbconnection = pg_connect($connectionString);
+	$result = pg_query($dbconnection,"SELECT password FROM useraccount WHERE username='".$username."'");
+	$row = pg_fetch_row($result);
+	$realPassword = $row[0];
+	return ($realPassword==$password) ? 'true' : 'false';
+}
 
 
-var Question = function(id, bodyText, answerText, image) {
-	// the question's id
-	this.id = id;
-
-	// the question body text.
-	this.bodyText = bodyText;
-
-	// a text representation of the answer. used for debugging. do not compare
-	// with supplied answer for testing user input
-	this.answerText = answerText;
-
-	// the type of question . eg multi, numebr, text
-	this.questionType;
-
-	// checks some supplied answer against the correct answer. returns bool
-	this.checkAnswer;
-
-	// function to convert a question to JSON format (may be unnecessary))
-	this.toJSON;
+function getQuizes(){
+	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
+	$dbconnection = pg_connect($connectionString);
+	$result = pg_query($dbconnection,"SELECT * FROM quiz;");
+//	$row = pg_fetch_all($result);
+	$bigline = "";
 	
-
-	//the url of the image to display.  leave blank if you want no picture
-	this.imageURL=image;
-
-
-	//funcftion to conver from JSON format to question
-//	this.fromJSON;
-
-};
-
-
-/**
- * A multi choice question that lets users select from the provided choices
- * (optionsText). The index of the correct option is provided too
- */
-var MultiChoiceQuestion = function(id, bodyText, answerIndex, optionsText, image) {
-	// call the super class constructor
-	Question.call(this, id, bodyText, optionsText[answerIndex], image);
-
-	this.optionsText = optionsText;
-	this.answerIndex = answerIndex;
-
-	this.questionType = "multi";
-
-	/**
-	 * Checks the answer against the supplied INDEX
-	 */
-	this.checkAnswer = function(answer) {
-		if (this.answerIndex == answer) {
-//			alert("Correct!");
-			return true;
-		} else {
-//			alert("Wrong answer.");
-			return false;
-
-		}
-	};
-
-	this.toJSON = function() {
-		var jobject = {};
-		jobject["id"] = id;
-		jobject["bodyText"] = bodyText;
-		jobject["optionsText"] = optionsText;
-		jobject["answerIndex"] = answerIndex;
-		jobject["image"]=image;
-		return jobject;
-
+	while($row = pg_fetch_array($result)){
+		$bigline .= "*";
+		$bigline .= $row['coursecode'] . ":" . $row['name'];
+	
 	}
 
+	return $bigline;
+}
 
 
-};
 
-/**
- * A question which asks the user for a number.
- */
-var NumberEntryQuestion = function(id, bodyText, answerNum, image) {
-	Question.call(this, id, bodyText, answerNum, image);
+function getAllQuestionsFromQuiz($quizname) {
+	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v 			user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
+	$dbconnection = pg_connect ( $connectionString );
+	$result = pg_query ( $dbconnection, "select row_to_json(row)
+from(SELECT * FROM quiz INNER JOIN question ON  (quiz.name=question.quizname) LEFT OUTER JOIN possibleanswers ON (question.panswerid = possibleanswers.id) WHERE(quiz.name='" . $quizname . "')) row;" );
+	return json_encode ( pg_fetch_all ( $result ) );
+}
 
-	this.questionType = "number";
 
-	this.answerText = Number(answerNum);
+function sendResults($username, $password, $quizname, $score) {
+	// get the user id
+	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v 			user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
+	$dbconnection = pg_connect ( $connectionString );
 
-	/**
-	 * compares the supplied number with the correct answer. Currently has no
-	 * tolerance, answers must be exact
-	 */
-	this.checkAnswer = function(answer) {
-		answer = parseAnswer(answer);
-		if (Number(answer) == NaN) {
-//			alert("Only numbers allowed.");
-			return false;
-		} else if (this.answerText == answer) {
-//			alert("Correct!");
-			return true;
-		} else {
-//			alert("Wrong answer.");
-			return false;
-		}
-	};
+	$id = pg_query ( $dbconnection, "SELECT id FROM useraccount WHERE username='$username'" );
+	$userid = pg_fetch_row ( $id )[0];
 
-	this.toJSON = function() {
-		var jobject = {};
-		jobject["id"] = id;
-		jobject["bodyText"] = bodyText;
-		jobject["answerNum"] = answerNum;
-		jobject["image"]=image;
-		return jobject;
-	};
+	pg_query ( $dbconnection, "INSERT INTO result (userid,quizname, score) VALUES ($userid,'" . $quizname . "',$score)" );
 
-//	this.fromJSON = function(jobject){
-//		this.answer = jobject["canswer"];
-//		this.body = jobject["body"];
-//	}
+	return;
+}
 
-};
+function getResults($username, $password) {
+	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v 			user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
+	$dbconnection = pg_connect ( $connectionString );
 
-/**
- * Just like a Number Entry Question but with a tolerance set for it.
- */
-var NumberEntryToleranceQuestion = function(id, bodyText, answerNum, tolerance, image) {
-	Question.call(this, id, bodyText, answerNum, image);
+	// get the users id number
+	$id = pg_query ( $dbconnection, "SELECT id FROM useraccount WHERE username='$username'" );
+	$userid = pg_fetch_row ( $id )[0];
 
-	this.questionType = "numTol";
+	$result = pg_query ( $dbconnection, " select row_to_json(row) from (SELECT * FROM result WHERE userid=$userid) row" );
 
-	this.tolerance = tolerance;
+	return json_encode ( pg_fetch_all ( $result ) );
+}
 
-	this.checkAnswer = function(answer) {
-		answer = parseAnswer(answer);
-		answer = parseFloat(answer);
-		answerNum = parseFloat(answerNum);
-		tolerance = parseFloat(tolerance);
 
-		console.log(answer);
-		console.log(answerNum);
-		console.log(Math.abs(answer-answerNum));
-		if (answer == NaN) {
-//			alert("Only numbers allowed.");
-			return false;
-		} else if (Math.abs(answer-answerNum) <= tolerance) {
-//			alert("Correct!");
-			return true;
-		}
-		else {
-//			alert("Wrong answer.");
-			return false;
-		}
+function getAllResults(){
+	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
+	$dbconnection = pg_connect($connectionString);
+	$result = pg_query($dbconnection,
+		  "SELECT u.fname, u.lname, u.id, r.quizname, r.score, r.timetaken, r.date 
+		  FROM result r 
+		  inner join useraccount u on r.userid=u.id 
+		  order by score desc");
+	return json_encode(pg_fetch_all($result));
+}
 
-	};
+function getQueryResults($fname,$lname,$quiz){
+	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
+	$dbconnection = pg_connect($connectionString);
+	
+	$result = pg_query($dbconnection,
+		"SELECT u.fname, u.lname, u.id, r.quizname, r.score, r.timetaken, r.date 
+		FROM result r 
+		inner join useraccount u on r.userid=u.id 		
+		where u.fname like '%$fname%'
+		and u.lname like '%$lname%'
+		and r.quizname like '%$quiz%'
+		order by score desc");
+	return json_encode(pg_fetch_all($result));
+}
 
-	this.toJSON = function() {
-		var jobject = {};
-		jobject["id"] = id;
-		jobject["bodyText"] = bodyText;
-		jobject["answerNum"] = answerNum;
-		jobject["image"]=image;
-		return jobject;
-	};
 
-};
-
-/**
- * Text question where users must enter an answer in string form.
- */
-var TextEntryQuestion = function(id, bodyText, answersArray, image) {
-	Question.call(this, id, bodyText, answersArray, image);
-
-	this.questionType = "text";
-
-	this.answersArray = answersArray;
-
-	/**
-	 * Checks the array of possible answers for the supplied text
-	 */
-	this.checkAnswer = function(answer) {
-		answer = parseAnswer(answer);
-		if (answersArray.indexOf(answer) != -1) {
-//			alert("Correct!");
-			return true;
-		} else {
-//			alert("Wrong answer.");
-			return false;
-		}
-	};
-
-	this.toJSON = function() {
-		var jobject = {};
-		jobject["id"] = id;
-		jobject["bodyText"] = bodyText;
-		jobject["answersArray"] = answersArray;
-		jobject["image"]=image;
-		return jobject;
-	};
-};
-
-/**
- * Test method for creating a series of questions
- */
-function dummyQuestions() {
-	MultiChoiceQuestion.prototype = Object.create(Question.prototype);
-	NumberEntryQuestion.prototype = Object.create(Question.prototype);
-	TextEntryQuestion.prototype = Object.create(Question.prototype);
-
-	var mcq = new MultiChoiceQuestion("NoID",
-			"Is this operation 'true' or 'false': \n 3 > 2", 0, [ "true",
-					"false" ]);
-
-	var neq = new NumberEntryQuestion("NoID", "How many legs does a human being have?",
-			2);
-
-	var acceptableAnswers = [ "feline", "cat", "hamster", "orange dog" ];
-	var teq = new TextEntryQuestion("NoID", "what is an orange furry pet",
-			acceptableAnswers);
-
-	var threeQuestions = [ mcq, neq, teq ];
-	for (var i = 0; i < threeQuestions.length; i++) {
-
-		// q.askQuesiton();
-		threeQuestions[i].checkAnswer("orange dog");
-
+if (isset ( $_POST ['funcName'] )) {
+	switch ($_POST ['funcName']) {
+		case 'checkPasswordAdmin' :
+			echo (checkPasswordAdmin ( $_POST ['username'], $_POST ['password'] ));
+			break;
+		case 'getAllQuestionsFromQuiz' :
+			echo (getAllQuestionsFromQuiz ( $_POST ['quizname'] ));
+			break;
+		case 'sendResults' :
+			echo (sendResults ( $_POST ['username'], $_POST ['password'], $_POST ['quizname'], $_POST 				['score'] ));
+			break;
+		case 'getResults' :
+			echo (getResults ( $_POST ['username'], $_POST ['password'] ));
+			break;
+		case 'checkPassword':
+			echo(checkPassword($_POST['username'],$_POST['password']));
+			break;
+		case 'getAllResults':
+			echo(getAllResults());
+			break;
+		case 'getQueryResults':
+			echo(getQueryResults($_POST['fname'],$_POST['lname'],$_POST['quiz']));
+			break;	
+		case 'getQuizzes':
+			echo(getQuizes());
+			break;
 	}
-};
+}
+?>
 
-/**
- *This should remove white spaces in user input
- *This makes it all small case
- *
- *Input: user's answer
- *Output: parsed string
- */
-function parseAnswer(userAnswer){
-	console.log("parse start");
 
-	//var userInput = $('#button').val();
-	//console.log(userInput);
-	var maniText = userAnswer;
-	maniText = maniText.toLowerCase();
-	maniText = maniText.trim();
 
-	var finalInput = maniText;
 
-	console.log(finalInput);
-	console.log("parse end");
 
-	return finalInput;
-};
