45a55228f00ee7eecc23219e539e6815876b3ffc
gekenel <alexc1@windowslive.com>
2014-09-11 10:24:18 +1200

    updated parser

--- processQuizEntry.php
+++ processQuizEntry.php
@@ -1,17 +1,34 @@
 <?php header('Access-Control-Allow-Origin: *');
 
-function InsertQuestion$quizname, $body, $panswerid, $canswer, $type,$tolerance,$imagename) {
+function InsertQuestion($quizname, $body, $panswerid, $canswer, $type,$tolerance,$imagename) {
 
 	return "test";
 
 	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
 	$dbconnection = pg_connect($connectionString);
 	//Insert Quiz values into DB
-	$result = pg_query($dbconnection,"INSERT INTO question (fname,lname,emailaddress,username,password,isadmin) VALUES ('$fname', '$lname', '$email', '$username', '$password', false);");
+	$result = pg_query($dbconnection,"INSERT INTO question (id,body,panswerid,canswer,type,tolerance,quizname,imagename) VALUES ('$quizname', '$body', '$panswerid', '$canswer', '$type', '$tolerance', '$imagename');");
 	if (!$result) {
 	  return"fail";
 	}else{
 	  return"success";
+	}
+	$row = pg_fetch_all($result);
+	return $row;
+}
+
+function InsertQuiz($name, $coursecode) {
+
+	return "test";
+
+	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
+	$dbconnection = pg_connect($connectionString);
+	//Insert Quiz values into DB
+	$result = pg_query($dbconnection,"INSERT INTO quiz (name, coursecode) VALUES ('$name', '$coursecode');");
+	if (!$result) {
+		return"fail";
+	}else{
+		return"success";
 	}
 	$row = pg_fetch_all($result);
 	return $row;
@@ -22,6 +39,9 @@ if (isset ( $_POST ['funcName'] )) {
 	switch ($_POST ['funcName']) {
 		case 'insertQuestion' :
 			echo (InsertQuestion ( $_POST ['quizname'], $_POST ['body'], $_POST ['panswerid'],$_POST ['canswer'],$_POST ['type'],$_POST ['tolerance'],$_POST ['imagename']);
+			break;
+		case 'insertQuiz' :
+			echo (InsertQuiz ( $_POST ['quizname'], $_POST ['coursecode']);
 			break;
 	}
 }
