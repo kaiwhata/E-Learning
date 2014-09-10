c72c5dfe024cf190685539e800d671feb4093bba
gekenel <alexc1@windowslive.com>
2014-09-11 11:56:09 +1200

    m

--- processQuizEntry.php
+++ processQuizEntry.php
@@ -1,13 +1,12 @@
 <?php header('Access-Control-Allow-Origin: *');
 
 function InsertQuestion($quizname, $body, $canswer, $type,$tolerance,$imagename) {
-
+	alert("got here");
 	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
 	$dbconnection = pg_connect($connectionString);
 	//Insert Quiz values into DB
 	$result = pg_query($dbconnection,"INSERT INTO question (body,canswer,type,tolerance,quizname,imagename) VALUES ('$body', '$canswer', '$type', '$tolerance', '$quizname', '$imagename');");
 	console.log("Values entered: " +$body+" "+$canswer+" "+$type);
-	alert("this is the result: "+$result);
 	if (!$result) {
 	  return"fail";
 	}else{
