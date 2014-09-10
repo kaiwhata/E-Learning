0a3ea53600e74acc755b819ff398cc94e330d7af
gekenel <alexc1@windowslive.com>
2014-09-11 11:25:10 +1200

    updated php files for quiz entry processing

--- processQuizEntry.php
+++ processQuizEntry.php
@@ -1,11 +1,13 @@
 <?php header('Access-Control-Allow-Origin: *');
 
-function InsertQuestion($quizname, $body, $panswerid, $canswer, $type,$tolerance,$imagename) {
+function InsertQuestion($quizname, $body, $canswer, $type,$tolerance,$imagename) {
 
 	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
 	$dbconnection = pg_connect($connectionString);
 	//Insert Quiz values into DB
-	$result = pg_query($dbconnection,"INSERT INTO question (body,panswerid,canswer,type,tolerance,quizname,imagename) VALUES ('$body', '$panswerid', '$canswer', '$type', '$tolerance', '$quizname', '$imagename');");
+	$result = pg_query($dbconnection,"INSERT INTO question (body,canswer,type,tolerance,quizname,imagename) VALUES ('$body', '$canswer', '$type', '$tolerance', '$quizname', '$imagename');");
+	console.log("Values entered: " +$body+" "+$canswer+" "+$type);
+
 	if (!$result) {
 	  return"fail";
 	}else{
@@ -36,7 +38,7 @@ if (isset ( $_POST ['funcName'] )) {
 
 	switch ($_POST ['funcName']) {
 		case 'insertQuestion' :
-			echo (InsertQuestion ( $_POST ['quizname'], $_POST ['body'], $_POST ['panswerid'],$_POST ['canswer'],$_POST ['type'],$_POST ['tolerance'],$_POST ['imagename']));
+			echo (InsertQuestion ( $_POST ['quizname'], $_POST ['body'],$_POST ['canswer'],$_POST ['type'],$_POST ['tolerance'],$_POST ['imagename']));
 			break;
 		case 'insertQuiz' :
 			echo (InsertQuiz ( $_POST ['quizname'], $_POST ['coursecode']));
