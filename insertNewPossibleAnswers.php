8e8796cdaabe605739c286dd93ce9d2b402a951c
gekenel <alexc1@windowslive.com>
2014-09-25 10:14:36 +1200

    hmmmm

--- insertNewPossibleAnswers.php
+++ insertNewPossibleAnswers.php
@@ -3,8 +3,11 @@ header ( 'Access-Control-Allow-Origin: *' );
 function insertNewPossibleAnswers($p1, $p2,$p3,$p4) {
 	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
 	$dbconnection = pg_connect ( $connectionString );
-
-	$result = pg_query ( $dbconnection, "INSERT INTO possibleanswers(p1,p2,p3,p4) VALUES ( $p1,$p2,$p3,$p4) RETURNING id;" );
+	if(is_numeric($p1)){
+		$result = pg_query ( $dbconnection, "INSERT INTO possibleanswers(p1,p2,p3,p4) VALUES ( $p1,$p2,$p3,$p4) RETURNING id;" );
+	}else{
+		$result = pg_query ( $dbconnection, "INSERT INTO possibleanswers(p1,p2,p3,p4) VALUES ( '$p1','$p2','$p3','$p4') RETURNING id;" );
+	}
 	$row = pg_fetch_row ( $result );
 	return $row [0];
 }
