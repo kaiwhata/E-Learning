88a8c45379122c338f76e8a356d857e0e908f5cd
gekenel <alexc1@windowslive.com>
2014-09-25 09:42:39 +1200

    hmmmm

--- js/adminQuizList/controllers.js
+++ js/adminQuizList/controllers.js
@@ -61,7 +61,7 @@ quizList.controller('adminQuizCtrl',function quizCtrl($scope) {
 			url : 'http://shrouded-earth-7234.herokuapp.com/deleteQuiz.php',
 			type : 'post',
 			data : {
-				"quizName" : quizName
+				"quizName" : quizName.trim()
 			},
 			success : function(response) {
 				alert(response);
