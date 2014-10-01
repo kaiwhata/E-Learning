133c5852268cfa0abdbaf999b5e15273ebe0907e
Sohaila Sahib <sahibsoha@petit-lyon.ecs.vuw.ac.nz>
2014-10-02 12:58:09 +1300

    hhhm

--- hasTakenQuiz.php
+++ hasTakenQuiz.php
@@ -1,3 +1,5 @@
+	<?php header('Access-Control-Allow-Origin: *');
+
 	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
 	$dbconnection = pg_connect ( $connectionString );
 
@@ -15,3 +17,5 @@
 	$numFound = $row [0];
 
 	return $numFound;
+
+	?>
