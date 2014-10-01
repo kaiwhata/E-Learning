df922d616bd81f118d09beb9a89b60219f89092a
Sohaila Sahib <sahibsoha@petit-lyon.ecs.vuw.ac.nz>
2014-10-02 12:42:54 +1300

    ssshhh

---
+++ hasTakenQuiz.php
@@ -0,0 +1,17 @@
+	$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
+	$dbconnection = pg_connect ( $connectionString );
+
+	$name = $_POST ['quizname'];
+	$quizname = $_POST ['name'];
+
+	//find the users id
+	$result = pg_query ( $dbconnection, "select id from useraccount where fname = '$name'" );
+	$row = pg_fetch_row ( $result );
+	$id = $row [0];
+
+	//use that id to try and find if there is a result
+	$result = pg_query ( $dbconnection,select count(*) from result where userid=1 AND quizname = '$quizname'");
+	$row = pg_fetch_row ( $result );
+	$numFound = $row [0];
+
+	echo $numFound;
