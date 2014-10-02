0fb853d38f198a6fc3defa5db207e48e9c5ae333
Chris Hawkins <chrishawkinsnz@gmail.com>
2014-10-02 14:33:40 +1300

    added phph to getch all courses

---
+++ resetPassword.php
@@ -0,0 +1,22 @@
+<?php
+header ( 'Access-Control-Allow-Origin: *' );
+
+$username = $_POST ['username'];
+$password = $_POST ['password'];
+
+$sql = "UPDATE useraccount SET password='$password' WHERE username='$username'";
+
+$connectionString = "host=ec2-54-225-101-64.compute-1.amazonaws.com port=5432 dbname=d1nigmib60rp1v user=jykiewmddlbjft password=kRqkD183znoOpPNTlDq6f_Xs29";
+$dbconnection = pg_connect ( $connectionString );
+$result = pg_query ( $dbconnection, $sql );
+$row = pg_fetch_row ( $result );
+$done = $row [0];
+
+echo $done;
+return;
+?>
+
+
+
+
+
