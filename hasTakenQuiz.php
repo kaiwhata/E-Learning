be870a1fc03b82e2edcdb351cba835371e825830
Sohaila Sahib <sahibsoha@petit-lyon.ecs.vuw.ac.nz>
2014-10-02 15:21:22 +1300

    fixed

--- hasTakenQuiz.php
+++ hasTakenQuiz.php
@@ -6,13 +6,15 @@
 	$name = $_POST ['name'];
 	$quizname = $_POST ['quizname'];
 
-	echo $name;
+
 
 	//find the users id
 	$result = pg_query ( $dbconnection, "select id from useraccount where username = '$name'" );
 	$row = pg_fetch_row ( $result );
 	$id = $row [0];
 
+	echo $id;
+	echo "----";
 	//use that id to try and find if there is a result
 	$result = pg_query ( $dbconnection,"select count(*) from result where userid=$id AND quizname = '$quizname'");
 	
